797291dd7081b3a0224ad4d09b200c57be36fe96
pacanada <pereirapcanada@gmail.com>
2022-10-08 13:20:00 +0200

    half working ui

--- src/playground/chess_ui_oop.py
+++ src/playground/chess_ui_oop.py
@@ -5,14 +5,14 @@ from chess_python.chess import Chess
 W = H = 480
 PIECE_IMAGE_DICT = {
     1: pyglet.resource.image("resources/white_pawn.png"),
-    2: pyglet.resource.image("resources/white_bishop.png"),
-    3: pyglet.resource.image("resources/white_knight.png"),
+    2: pyglet.resource.image("resources/white_knight.png"),
+    3: pyglet.resource.image("resources/white_bishop.png"),
     4: pyglet.resource.image("resources/white_rook.png"),
     5: pyglet.resource.image("resources/white_queen.png"),
     6: pyglet.resource.image("resources/white_king.png"),
     -1: pyglet.resource.image("resources/black_pawn.png"),
-    -2: pyglet.resource.image("resources/black_bishop.png"),
-    -3: pyglet.resource.image("resources/black_knight.png"),
+    -2: pyglet.resource.image("resources/black_knight.png"),
+    -3: pyglet.resource.image("resources/black_bishop.png"),
     -4: pyglet.resource.image("resources/black_rook.png"),
     -5: pyglet.resource.image("resources/black_queen.png"),
     -6: pyglet.resource.image("resources/black_king.png"),
@@ -39,7 +39,7 @@ def from_index_to_coord(index:int)->Tuple[int,int]:
 
 
 
-#pieces = generate_sprites_from_board(board=game.state.board)
+DEBUG = True
 
 class ChessBoard(pyglet.window.Window):
     def __init__(
@@ -47,15 +47,18 @@ class ChessBoard(pyglet.window.Window):
         width: int,
         height: int,
         name: str = "Window",
-        dt: float = 2,
+        dt: float = 10,
 
     ):
         super().__init__(width, height, name, resizable=False)
         self.set_vsync(False)
-        pyglet.clock.schedule_interval(self.update, dt)
+        #pyglet.clock.schedule_interval(self.update, dt)
         self.fps_display = pyglet.window.FPSDisplay(window=self)
         self.game = Chess()
         self.background = pyglet.resource.image("resources/board.svg.png")
+        self.pos_i = None
+        self.pos_f = None
+        self.allowed_moves_in_pos = []
 
     def draw_sprites_from_board(self):
 
@@ -69,33 +72,54 @@ class ChessBoard(pyglet.window.Window):
                 pieces.append(piece_sprite)
         batch.draw()
 
+    def draw_allowed_moves(self):
+        batch_circle = pyglet.graphics.Batch()
+        #allowed_moves = self.game.legal_moves_in_position(pos=pos)
+        circles = []
+        for index in self.allowed_moves_in_pos:
+            x,y = from_index_to_coord(index)
+            print(x,y)
+            circle = pyglet.shapes.Circle(x, y, 8, color=(33,39,33), batch=batch_circle)
+
+            circles.append(circle)
+        batch_circle.draw()
+
 
     def on_draw(self):
         self.clear()
         self.background.blit(0,0)
         self.draw_sprites_from_board()
+        self.draw_allowed_moves()
 
 
-    def update(self, dt):
-        self.clear()
-        print("updating")
+    def on_key_press(self, symbol, modifier): 
+        # key "Q" get press
+        if symbol == pyglet.window.key.Q:
+            # close the window
+            self.close()
 
     def on_mouse_press(self, x, y, button, modifiers):
+        if self.pos_i is None:
+            self.pos_i = from_coord_to_index(x,y)
+            self.allowed_moves_in_pos = self.game.legal_moves_in_position(pos=self.pos_i)
+            self.draw_allowed_moves(self.pos_i)
+        else:
+            self.pos_f = from_coord_to_index(x,y)
+            self.game.move(move=[self.pos_i, self.pos_f, None], check_allowed_moves=True)
+            print(self.game)
+            self.draw_sprites_from_board()
+            self.pos_i = None
+            self.pos_f = None
+        if DEBUG:
+            print(f"Moused pressed {x=}, {y=}, {button=}, {modifiers=}", )
+            print(f"Corresponding to index: {self.pos_i}")
 
-        pos_i = from_coord_to_index(x,y)
-        print(f"Moused pressed {x=}, {y=}, {button=}, {modifiers=}", )
-        print(f"Corresponding to index: {pos_i}")
-
-    def on_mouse_release(x, y, button, modifiers):
-        global pos_f
-        pos_f = from_coord_to_index(x,y)
-        print(pos_i, pos_f)
-        game.move(move=[pos_i, pos_f, None], check_allowed_moves=True)
-        print(game)
-        pieces = generate_sprites_from_board(board=game.state.board)
-        # background.blit(0,0)
-        # for piece in pieces:
-        #     piece.draw()
+    def on_mouse_release(self, x, y, button, modifiers):
+        #pos_f = from_coord_to_index(x,y)
+        #print(self.pos_i, pos_f)
+        #self.game.move(move=[self.pos_i, pos_f, None], check_allowed_moves=True)
+        #print(self.game)
+        #self.draw_sprites_from_board()
         print(f"Moused realeased {x=}, {y=}, {button=}, {modifiers=}", )
 
     def on_mouse_drag(self,x, y, dx, dy, buttons, modifiers):
