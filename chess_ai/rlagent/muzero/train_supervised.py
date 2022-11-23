8a488370fec8be86f16c240559b5fb0b28152f81
pacanada <pereirapcanada@gmail.com>
2022-11-23 21:24:21 +0100

    some dev

--- chess_ai/rlagent/muzero/train_supervised.py
+++ chess_ai/rlagent/muzero/train_supervised.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from torch import nn
@@ -11,14 +12,19 @@ from chess_ai.rlagent.muzero.utils import BufferDataset, get_root_dir, process_b
 
 
 batch_size = 1000
-epochs = 5
-buffer = pd.concat([pd.read_feather("supervised_dataset_1.feather"), pd.read_feather("supervised_dataset_2.feather")] )
+epochs = 100
+buffer = pd.DataFrame()
+for i in range(2):
+    buffer_ = pd.read_feather(get_root_dir() / "data" / "dataframe" / f"buffer_{i+1}_df.feather")
+    buffer = pd.concat([buffer, buffer_])
+
+buffer_1 = pd.read_feather(get_root_dir() / "data" / "dataframe" / "buffer_1_df.feather")
 #buffer["value_all"] = buffer.evaluation.apply(lambda x: x["value"] if x["type"]=="cp" else x["value"]*10000)
-buffer["value"] = 1/(1+np.exp(-0.01*buffer.evaluation))
+#buffer["value"] = 1/(1+np.exp(-0.01*buffer.evaluation))
 #buffer["mate_value"] = buffer.evaluation.apply(lambda x: x["value"] if x["type"]=="mate" else None)
 model = AlphazeroNetSupervised()
-model.load_state_dict(torch.load(get_root_dir() / "checkpoints/nn_supervised.pth"))
-model.eval()
+# model.load_state_dict(torch.load(get_root_dir() / "checkpoints/nn_supervised.pth"))
+# model.eval()
 x, y_value, y_policy = process_buffer_to_torch(buffer)
 dataset = BufferDataset(x=x,y_value=y_value, y_policy=y_policy)
 train_dataloader = DataLoader(dataset=dataset, shuffle=True, batch_size=batch_size)
@@ -42,4 +48,8 @@ for it in range(epochs):
 
     loss_list.append(loss.mean().detach().numpy())
     print(it, loss.mean())
+    if it%20==0:
+        torch.save(model.state_dict(), get_root_dir() /"checkpoints/nn_supervised.pth")
+        print("saving")
 torch.save(model.state_dict(), get_root_dir() /"checkpoints/nn_supervised.pth")
+plt.plot(loss_list)
